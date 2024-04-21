8aa448de61393b8e85ee12b2f934b505a4bbf9b1
Shiraz Waseem <shirazwaseem321@gmail.com>
2024-04-22 00:34:22 +0500

    Add User and Chats Displaying added

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,21 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./addUser.css"
+import { useUserStore } from '../../../../lib/userStore';
+import { arrayUnion, collection, doc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore"
+import { db } from "../../../../lib/firebase";
+
+
 const AddUser = () => {
+    const [user, setUser] = useState(null);
+    const { currentUser } = useUserStore();
+
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get("username");
+
+        try {
+            const userRef = collection(db, "users");
+            const q = query(userRef, where("username", "==", username));
+            const querySnapshot = await getDocs(q);
+
+            if (!querySnapshot.empty) {
+                // return the first docs and return it to data
+                setUser(querySnapshot.docs[0].data());
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    const handleAdd = async () => {
+        // Creating new chat
+        const chatRef = collection(db, "chats");
+        const userChatsRef = collection(db, "userchats");
+
+        try {
+            // Lets add a new chat
+            const newChatRef = doc(chatRef);  // we need id is lia variable bana ke kia. A reference is created
+
+            await setDoc(newChatRef, {
+                createdAt: serverTimestamp(),
+                messages: [],
+            });
+
+            // console.log(newChatRef.id)  // ChatId
+
+            // We will pass here the userId (Jo banda search kia)
+            await updateDoc(doc(userChatsRef, user.id), {
+                // Arrayunion simple push in array, in arrayUnion you cant use serverTimeStamp
+
+                // Here the userchat ko it will pass our id at receiver
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: "",
+                    receiverId: currentUser.id,
+                    updatedAt: Date.now(),
+                })
+            })
+            // For us
+            // For our chats
+
+            await updateDoc(doc(userChatsRef, currentUser.id), {
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: "",
+                    receiverId: user.id,
+                    updatedAt: Date.now(),
+                }),
+            });
+
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
     return (
         <div className='addUser'>
-            <form>
+            <form onSubmit={handleSearch}>
                 <input type="text" placeholder="Username" name="username" />
                 <button>Search</button>
             </form>
 
             {/* User */}
-            <div className="user">
-                <div className="detail">
-                    <img src="./images/avatar.png" alt="" />
-                    <span>John Doe</span>
+            {user && (
+                <div className="user">
+                    <div className="detail">
+                        <img src={user.avatar || "./images/avatar.png"} alt="" />
+                        <span>{user.username}</span>
+                    </div>
+                    <button onClick={handleAdd}>Add User</button>
                 </div>
-                <button>Add User</button>
-            </div>
+            )}
         </div>
     )
 }
--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,9 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./chatlist.css"
 import AddUser from './addUser/addUser';
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../lib/firebase';
 
 const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
+    const { currentUser } = useUserStore();
+    const [chats, setChats] = useState([]);
+
+
+    useEffect(() => {
+        // const unSub = onSnapshot(doc(db, "userchats", currentUser.id), (doc) => {
+        //     // console.log("Current data: ", doc.data());
+        //     setChats(doc.data())
+        // });
+
+
+
+        const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+            const items = res.data().chats;
+
+            // We got a list
+            const promises = items.map(async (item) => {
+                // Fetching data through receiverId we got from userchats
+                // Jis id sy receive huwa unki chats hi aye gy na
+                const userDocRef = doc(db, "users", item.receiverId);
+                const userDocSnap = await getDoc(userDocRef);
+
+                const user = userDocSnap.data();
+
+                // returning all the chatinfo through item and the user too.
+                return { ...item, user };
+            })
+
+            const chatData = await Promise.all(promises);
+            // setChats(chatData);
+
+            // To show latest chats we will sort
+            setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+        });
+
+        // cleanup
+        return () => {
+            unSub();
+        };
+    }, [currentUser.id])
+
+    console.log("Chats are: ", chats)
 
     return (
 
@@ -33,106 +78,21 @@ const ChatList = () => {
                 />
             </div>
 
-            {/* Displaying chats. One Item is one chat */}
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
+            {/* Displaying chats.  */}
+            {chats.map((chat) => (
+                <div className="item" key={chat.chatId}>
+                    <img
+                        src=
+                        "./images/avatar.png"
+                        alt=""
+                    />
+                    <div className="texts">
+                        {/* Jiska msg uska name */}
+                        <span>{chat.user.username}</span>
+                        <p>{chat.lastMessage}</p>
+                    </div>
                 </div>
-            </div>
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
-            <div className="item">
-                <img
-                    src=
-                    "./images/avatar.png"
-                    alt=""
-                />
-                <div className="texts">
-                    {/* Jiska msg uska name */}
-                    <span>John Doe</span>
-                    {/* Last message hi dhikata ha na */}
-                    <p>Hello for now</p>
-                </div>
-            </div>
-
+            ))}
             {addMode && <AddUser />}
 
         </div>
